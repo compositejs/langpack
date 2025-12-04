c6af8d86720623cbd4ecfea6cf98ed498d5166e1
Kingcean <royaljinchen@msn.com>
2025-12-05 00:25:58 +0800

    Enhance locale strings.

--- src/core.ts
+++ src/core.ts
@@ -80,4 +80,68 @@ export function getQueryInfo(url?: string, options?: QueryInfoResolveOptionsCont
     return result;
 }
 
+export function getLocaleProp<T = any>(obj: T, key?: keyof(T) | null, options?: {
+    mkt?: string | boolean;
+    fallback?: any;
+    bind?: any;
+}) {
+    if (!obj) return options?.fallback || undefined;
+    let mkt: string;
+    const b = options?.bind || {};
+    if (typeof options?.mkt === "string") mkt = options?.mkt;
+    if (!mkt && options?.mkt !== false && typeof navigator === "object") mkt = navigator.language;
+    if (!key) key = "name" as any;
+    if (typeof key !== "string") return getKeyedPropValue(obj, key as any, undefined, options);
+    if (!mkt || typeof mkt !== "string") return getKeyedPropValue(obj, key, undefined, options);
+    let k = `${key}#${mkt}`;
+    let v = (obj as any)[k];
+    if (v !== undefined) {
+        b.key = k;
+        return v;
+    }
+
+    const arr = mkt.split("-");
+    if (arr.length < 2) return getKeyedPropValue(obj, key, undefined, options);
+    if (arr.length === 2) return getKeyedPropValue(obj, `${key}#${arr[0]}`, key, options);
+    const special = `${key}#${arr[0]}-${arr[2]}`;
+    while (arr.length > 2) {
+        arr.pop();
+        k = "";
+        for (let j = 0; j < arr.length; j++) {
+            v = arr[j];
+            if (!v) continue;
+            if (k) k += "-";
+            k += v;
+        }
+
+        v = (obj as any)[`${key}#${k}`];
+        if (v !== undefined) return v;
+    }
+
+    v = (obj as any)[special];
+    if (v !== undefined) {
+        b.key = special;
+        return v;
+    }
+
+    return getKeyedPropValue(obj, `${key}#${arr[0]}`, key, options);
+}
+
+function getKeyedPropValue(obj: any, key1: string, key2: string, options: {
+    bind?: any;
+    fallback?: any;
+}) {
+    let key = key1;
+    let v = obj[key];
+    if (v === undefined && key2) {
+        key = key2;
+        v = obj[key2];
+    }
+
+    if (v === undefined) return options?.fallback;
+    const b = options?.bind;
+    if (b) b.key = key;
+    return v;
+}
+
 }
